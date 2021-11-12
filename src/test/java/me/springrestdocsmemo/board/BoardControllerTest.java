555882995c6261ba10416e39807915495d2f355c
shineoov <shineoov88@gmail.com>
2021-11-12 22:44:13 +0900

    docs 기본 생성

--- src/test/java/me/springrestdocsmemo/board/BoardControllerTest.java
+++ src/test/java/me/springrestdocsmemo/board/BoardControllerTest.java
@@ -1,19 +1,29 @@
 package me.springrestdocsmemo.board;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationContextProvider;
+import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@ExtendWith(RestDocumentationExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 class BoardControllerTest {
@@ -24,6 +34,15 @@ class BoardControllerTest {
     @Autowired
     ObjectMapper objectMapper;
 
+    @Autowired
+    WebApplicationContext webApplicationContext;
+
+    @BeforeEach
+    public void setUp(RestDocumentationContextProvider restDocumentation){
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+                .apply(documentationConfiguration(restDocumentation).uris())
+                .build();
+    }
 
     @Test
     public void saveBoard() throws Exception {
@@ -42,6 +61,7 @@ class BoardControllerTest {
                 .andExpect(jsonPath("title").exists())
                 .andExpect(jsonPath("writer").exists())
                 .andDo(print())
+                .andDo(document("board-save"))
         ;
     }
 }
