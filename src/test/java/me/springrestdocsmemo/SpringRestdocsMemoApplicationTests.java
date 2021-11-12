33ae906c4b3f1a8e6bf8dfaa687f9f5bf1f8e1a4
shineoov <shineoov88@gmail.com>
2021-11-12 21:08:07 +0900

    project initialize

---
+++ src/test/java/me/springrestdocsmemo/SpringRestdocsMemoApplicationTests.java
@@ -0,0 +1,13 @@
+package me.springrestdocsmemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestdocsMemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
